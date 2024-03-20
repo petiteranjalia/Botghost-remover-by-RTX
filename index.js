8ca39389d5ef5160506472b35f80aa8e14bdd1dd
TheJellyFishGuy <141530454+TheJellyFishGuy@users.noreply.github.com>
2024-03-20 12:26:47 +0100

    Updated index.js
    Looks better now

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ app.get('/', (req, res) => {
   res.send('YaY Your Bot Status Changed✨');
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
-  console.log(`🔗 Powered By RTX`);
+  console.log(`Listening to TheJellyFishGuy: http://localhost:${port}`);
+  console.log(`Powered By TheJellyFishGuy`);
 });
 
 
@@ -91,9 +91,7 @@ function updateStatusAndSendMessages() {
 }
 
 client.once('ready', () => {
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ✨HAPPY NEW YEAR MY DEAR FAMILY`);
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️WELCOME TO 2024`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    Bot logged in as ${client.user.tag} \^o^/`);
   updateStatusAndSendMessages();
 
   setInterval(() => {
